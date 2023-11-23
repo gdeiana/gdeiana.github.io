7122852c68c8c8cdd851367d8779dcaddf803b8d
guidodeiana <deianaguido@gmail.com>
2023-11-22 21:09:14 -0500

    new google analytics test2 + rebuild

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Run Stata in VS Code",
-        "excerpt":"If you are fed up with Stata’s do file editor and want something lighter, smarter and just better you are in the right place. Disclaimer This guide is heavily based on a guide I found online a few months ago. As I was not able to find it again I...","categories": ["Economics"],
+        "excerpt":"Why running Stata in Visual Studio Code Economists and statisticians often tend to use softwares (such as Stata or Matlab) as they are, without questioning the environment in which they code. However we can learn a lot from professional programmers, who use more advanced tools and better overall environments to...","categories": ["Economics"],
         "tags": ["Software","Stata"],
         "url": "/economics/stata-vscode/",
+        "teaser": null
+      },{
+        "title": "Personal Empirical Project Setup",
+        "excerpt":"This is a very short guide on how to setup a project involving codes and data for either economic analysis (industry) or research (academia). This fully draws from my limited experience working in different projects in government organizations, private companies and academia. The code is still in full development, please...","categories": ["Economics"],
+        "tags": ["Software","Project","Data"],
+        "url": "/economics/projects-guide/",
         "teaser": null
       }]
